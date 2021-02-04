05e23f56c68426b52453662404690fe8aa1d9511
03242020 <62064207+03242020@users.noreply.github.com>
2021-02-04 21:10:42 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+include "common.php";
+session_start();
+
+// checkEmail関数のテスト(メルアドチェック関数@test.co.jpになっているか)
+$_SESSION[‘email’] = ”cockatiehotest.co.jp”;
+checkEmail($_SESSION['email']);
+
+//checkLen関数のテスト(左に文字列、右に数値)
+$_SESSION['title'] = "inomata";
+
+checkLen($_SESSION['title'],"8");
+
+// isBlank関数のテスト(空欄チェック)
+$_SESSION['text'] = "z";
+isBlank('text');
+echo $_SESSION['text'];
+?>
